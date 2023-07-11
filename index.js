fb0336d286a49ce7fe10e3f75fc87b8f6f6400fc
Deborah Kurz <DeborahKurz6@gmail.com>
2023-07-11 13:12:51 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+function saturdayFun(activity){
+    if(activity){
+        return (`This Saturday, I want to ${activity}!`);
+    }else{
+        return (`This Saturday, I want to roller-skate!`);
+    }
+    
+}
+
+function mondayWork(place) {
+    if(place){
+        return(`This Monday, I will ${place}.`);
+    }else{
+        return(`This Monday, I will go to the office.`);
+    }
+ }
+ 
+ function wrapAdjective(part1){
+    return function wrapAdjective(saySomething) {
+        return (`You are ${part1}${saySomething}${part1}!`);
+    };
+ };
